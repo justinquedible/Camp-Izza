ba40244f76706c55d2f5c297dd0dc36b644b4f98
Justin Quach <justinquedible@gmail.com>
2022-01-17 01:53:42 -0800

    Format Footer

--- campizza-backend/frontend/src/Footer.tsx
+++ campizza-backend/frontend/src/Footer.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import { Container, Row, Col} from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-
-const FooterInstance =(
-    <div className="footer">
-        <Container fluid>
-            <Row>
-                    <div className="textbox">
-                        <Container fluid className="mainText">
-                                Camp Izza is a 501 (c)(3) non-profit organization
-                                registered in the state of California with federal tax ID #26-2174441.
-                        </Container>
-                        <Container fluid>
-                                PO Box 50326, Irvine CA, 92690 • (949) 422-8123
-                                <br /> © 2020 Camp Izza
-                        </Container>
-                    </div>
-            </Row>
-        </Container>
-    </div>
-
+const FooterInstance = (
+  <div className="footer">
+    <Container fluid>
+      <Row>
+        <div className="textbox">
+          <Container fluid className="mainText">
+            Camp Izza is a 501 (c)(3) non-profit organization registered in the
+            state of California with federal tax ID #26-2174441.
+          </Container>
+          <Container fluid>
+            PO Box 50326, Irvine CA, 92690 • (949) 422-8123
+            <br /> © 2020 Camp Izza
+          </Container>
+        </div>
+      </Row>
+    </Container>
+  </div>
 );
-
-
 
 // const FooterInstance =(
 //     <div className="footer">
@@ -30,9 +26,5 @@ const FooterInstance =(
 //     </div>
 //
 // );
-
-
-
-
 
 export default FooterInstance;
